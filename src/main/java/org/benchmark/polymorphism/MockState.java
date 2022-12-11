25375ae7ccbb60ed7a3e5b6ac6e1349d886a4acc
advasileva <Alenadvasileva@yandex.ru>
2022-12-12 02:17:51 +0300

    fix: update logic

--- src/main/java/org/benchmark/polymorphism/MockState.java
+++
@@ -1,9 +0,0 @@
-package org.benchmark.polymorphism;
-
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.State;
-
-@State(Scope.Thread)
-public class MockState {
-    public int price = 123;
-}
