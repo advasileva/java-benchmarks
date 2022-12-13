b7be6bd176724982753550af3397a813bd3ef17a
advasileva <Alenadvasileva@yandex.ru>
2022-12-13 13:30:00 +0300

    feat: use random

--- src/main/java/org/benchmark/polymorphism/Book.java
+++ src/main/java/org/benchmark/polymorphism/Book.java
@@ -1,12 +1,15 @@
 package org.benchmark.polymorphism;
 
+import java.util.Random;
+
 public final class Book extends Product {
+    private Random rand = new Random();
     public Book(String title) {
         super(title);
     }
 
     @Override
     public int price() {
-        return 40;
+        return rand.nextInt();
     }
 }
--- src/main/java/org/benchmark/polymorphism/Movie.java
+++ src/main/java/org/benchmark/polymorphism/Movie.java
@@ -1,12 +1,15 @@
 package org.benchmark.polymorphism;
 
+import java.util.Random;
+
 public final class Movie extends Product {
+    private Random rand = new Random();
     public Movie(String title) {
         super(title);
     }
 
     @Override
     public int price() {
-        return 15;
+        return rand.nextInt();
     }
 }
